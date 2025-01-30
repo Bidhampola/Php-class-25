0a84cb8d577216d0d0c31dacac5f51899d21b158
Bidhampola <brianemperor14@gmail.com>
2025-01-30 21:58:54 +0300

    initial commit

---
+++ intro1.php
@@ -0,0 +1,47 @@
+<?php 
+
+//printing out
+echo "Welcome to php";
+
+
+//new line
+echo "<br>";
+
+// php variables and data types
+$_fname = "Jimmy";
+$lname = "Daniel";
+// $age = 56;
+$amunt = 49.00;
+$isAlive = true;
+
+$qualification = null;
+
+$qualification = "Degree";
+
+const AGE = 50; //using const
+// AGE = 60;
+print(AGE);
+
+//printin variables
+echo $fname;
+
+//concatenation or joining variable to be printed
+echo "<br>";
+echo "My name is ".$lname. " and I am ". $age. " old";
+echo "<br>";
+
+echo $fname ." ". $lname;
+echo "<br>";
+
+$num1 = 50;
+$num2 = 40;
+
+$sum = $num1 + $num2;
+echo $sum;
+
+echo 2**3
+
+
+
+
+?>
